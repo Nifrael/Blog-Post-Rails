58bb0be4f2992d38fa16ac1716438e44fd857484
Julien Marchand <nifrael66@gmail.com>
2024-07-07 16:59:09 +0200

    Edit actions

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -18,13 +18,32 @@ class BlogPostsController < ApplicationController
     if @blog_post.save
       redirect_to @blog_post
     else
-      render :new
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def edit
+    @blog_post = BlogPost.find(params[:id])
+  end
+
+  def update
+    @blog_post = BlogPost.find(params[:id])
+    if @blog_post.update(blog_post_params)
+      redirect_to @blog_post
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @blog_post = BlogPost.find(params[:id])
+    @blog_post.destroy
+    redirect_to root_path
   end
 
   private
 
   def blog_post_params
-    params.requite(:blog_post).permit(:title, :body)
+    params.require(:blog_post).permit(:title, :body)
   end
 end
